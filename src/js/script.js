b0273dd25affe117145d3f8fd6e8a26738b37147
Lacroix-Eva <eva.lacroix@edu.univ-fcomte.fr>
2024-11-20 15:21:41 +0100

    Re-correction : Défilement en fonction de la largeur d'une image

--- src/js/script.js
+++ src/js/script.js
@@ -5,17 +5,18 @@ const prevButton = document.querySelector(".carousel__button--prev");
 const nextButton = document.querySelector(".carousel__button--next");
 
 const premierItem = document.querySelector(".carousel__item");
-const scrollAmount = premierItem.clientWidth;
 
 // Largeur de défilement d’un item
 if (carousel) {
   // Scroll au clic sur le bouton précédent
   prevButton.addEventListener("click", () => {
+    const scrollAmount = premierItem.clientWidth;
     carousel.scrollBy({ left: -scrollAmount, behavior: "smooth" });;
   });
 
   // Scroll au clic sur le bouton suivant
   nextButton.addEventListener("click", () => {
+    const scrollAmount = premierItem.clientWidth;
     carousel.scrollBy({ left: +scrollAmount, behavior: "smooth" });;
   });
 }
